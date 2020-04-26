9f4b5f6a28e2fba4b4176f058686632c4513a6a5
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-26 14:53:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,12 @@ def monopoly_with_second_tier
       :railroads => {
         :pieces=> 4 ,
         :rent_in_dollars=> {},
-        :names=> {}
+        :names=> {
+          :reading_railroad=>{},
+          :pennsylvania_railroad=>{},
+          :b_and_o_railroad=>{},
+          :shortline_railroad=>{}
+}
 }
 
   }
@@ -26,7 +31,12 @@ def monopoly_with_third_tier
           :two_pieces_owned=>50,
           :three_pieces_owned=>100,
           :four_pieces_owned=>200},
-        :names=> {}
+        :names=> {
+          :reading_railroad=>{},
+          :pennsylvania_railroad=>{},
+          :b_and_o_railroad=>{},
+          :shortline_railroad=>{}
+}
 }
 
   }
